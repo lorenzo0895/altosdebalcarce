fae0d7bc1e0dd49fef6d53bd5a60f8fcf738dd0e
Lorenzo Spallione <lorenzo0895@gmail.com>
2022-01-09 15:33:19 -0300

    Subido

--- routes/login.js
+++ routes/login.js
@@ -13,19 +13,24 @@ router.get('/login', (req, res) => {
 
 router.post('/login', async (req, res) => {
   const { dni, password } = req.body;
-  const user = await User.findOne({ dni : dni });
-  if (!user) {
-    res.redirect('/login?msj=El usuario no existe.');
-  } else {
-    const coinciden = await bcrypt.compare(password, user.password);
-    if (!coinciden) {
-      res.redirect('/login?msj=La contraseña es incorrecta.');
+  try {
+    const user = await User.findOne({ dni: dni });
+    if (!user) {
+      res.redirect('/login?msj=El usuario no existe.');
     } else {
-      req.session.dni = user.dni;
-      req.session.admin = user.admin;
-      res.redirect('/');
+      const coinciden = await bcrypt.compare(password, user.password);
+      if (!coinciden) {
+        res.redirect('/login?msj=La contraseña es incorrecta.');
+      } else {
+        req.session.dni = user.dni;
+        req.session.admin = user.admin;
+        res.redirect('/');
+      }
     }
+  } catch (error) {
+    res.redirect('/login');
   }
+
 });
 
 router.get('/logout', (req, res) => {
