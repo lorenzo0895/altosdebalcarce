af15ca5b5b45f8116075cabfc79fec5e449c60c1
Lorenzo Spallione <lorenzo0895@gmail.com>
2022-01-10 13:41:06 -0300

    arreglos turnos servicio

--- services/turnos.js
+++ services/turnos.js
@@ -2,7 +2,7 @@ const Horarios = require('../models/Horarios');
 
 module.exports.getTurnosFuturos = async function (seccion) {
   let now = new Date();
-  let diasLimite = 3;
+  let diasLimite = 2;
   let horas = await Horarios.find({ seccion: seccion }).sort('dia').sort('desdeHoras').sort('desdeMinutos');
   let objeto = {
     0: [],
@@ -17,22 +17,46 @@ module.exports.getTurnosFuturos = async function (seccion) {
     objeto[el.dia].push(el);
   });
   let objetoHorarios = {};
-  for (let i = 0; i < diasLimite; i++) {
+  for (let i = 0; i <= diasLimite; i++) {
     objetoHorarios[i] = [];
   }
 
-  //----- GUARDAMOS OBJETOS EN objetoHorarios[i]. i VA A IR DESDE CERO HASTA diasLimite-1 -----
-  for (let i = 0; i < diasLimite; i++) {
+  //----- GUARDAMOS OBJETOS EN objetoHorarios[i]. i VA A IR DESDE CERO HASTA diasLimite -----
+  for (let i = 0; i <= diasLimite; i++) {
     let diaComparable = new Date();
-    diaComparable.setDate(diaComparable.getDate() - 1 + i);
-    diaComparable = diaComparable.getDay();
+    let brechaHoraria = diaComparable.getHours() - diaComparable.getUTCHours();
+    diaComparable.setHours(diaComparable.getHours() + brechaHoraria);
+    diaComparable.setDate(diaComparable.getDate() + i);
+    diaComparableNumero = diaComparable.getDay() - 1;
 
-    objeto[diaComparable].forEach(el => {
-      objetoHorarios[i].push(el);
+    objeto[diaComparableNumero].forEach(el => {
+      let horasLimiteHasta = el.hastaHoras;
+      let minutosLimiteHasta = el.hastaMinutos;
+      let horasLimiteDesde = el.desdeHoras;
+      let minutosLimiteDesde = el.desdeMinutos;
+      let hastaComparable = new Date();
+      hastaComparable.setHours(brechaHoraria + horasLimiteHasta, minutosLimiteHasta, 0);
+      let desdeComparable = new Date();
+      desdeComparable.setHours(brechaHoraria + horasLimiteDesde, minutosLimiteDesde, 0);
+      desdeComparable.setDate(desdeComparable.getUTCDate() + diasLimite);
+
+      //FILTRAMOS HORARIOS QUE YA PASARON EL DÍA DE HOY
+      if (i === 0) {
+        if (hastaComparable >= diaComparable) {
+          objetoHorarios[i].push(el);
+        }
+      } else if (i === diasLimite) {
+        //FILTRAMOS HORARIOS DEL ÚLTIMO DÍA QUE EXCEDEN EL LÍMITE
+        if (desdeComparable <= diaComparable) {
+          objetoHorarios[i].push(el);
+        }
+      } else {
+        objetoHorarios[i].push(el);
+      }
     });
   }
 
   return objetoHorarios;
 
-  
+
 }
